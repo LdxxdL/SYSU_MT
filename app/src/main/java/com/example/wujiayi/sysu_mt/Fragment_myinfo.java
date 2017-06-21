1bf980ecf7ecd72bbbe00a4706a11a26f42f0c4f
wujiayi <wujiayigg666@gmail.com>
2017-06-21 20:07:41 +0800

    底部菜单

---
+++ app/src/main/java/com/example/wujiayi/sysu_mt/Fragment_myinfo.java
@@ -0,0 +1,21 @@
+package com.example.wujiayi.sysu_mt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Wujiayi on 2017/6/21.
+ */
+
+public class Fragment_myinfo extends Fragment
+
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item3, null);
+        return view;
+    }
+}
