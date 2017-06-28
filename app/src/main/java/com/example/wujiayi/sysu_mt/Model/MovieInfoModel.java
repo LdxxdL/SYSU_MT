138cfbfe376c79d0e58f053a45b14f574a0a4731
wujiayi <wujiayigg666@gmail.com>
2017-06-28 21:55:09 +0800

    getMovieList

---
+++ app/src/main/java/com/example/wujiayi/sysu_mt/Model/MovieInfoModel.java
@@ -0,0 +1,21 @@
+package com.example.wujiayi.sysu_mt.Model;
+
+/**
+ * Created by Wujiayi on 2017/6/28.
+ */
+
+public class MovieInfoModel {
+    public movieinfo movieview;
+
+    public MovieInfoModel(movieinfo movieview) {
+        this.movieview = movieview;
+    }
+
+    public void getMovieInfo() {
+
+    }
+
+    public void setMovieImage() {
+
+    }
+}
---
+++ app/src/main/java/com/example/wujiayi/sysu_mt/Model/MovieListModel.java
@@ -0,0 +1,13 @@
+package com.example.wujiayi.sysu_mt.Model;
+
+import java.util.List;
+
+/**
+ * Created by Wujiayi on 2017/6/28.
+ */
+
+public class MovieListModel {
+    public List<movieinfo> movieList;
+
+
+}
